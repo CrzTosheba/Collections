af2c31a367a58923ce66a6749d09b7d74fb08ff2
CrzTosheba <fugu@list.ru>
2022-04-28 20:17:56 +0300

    Добавьте файлы проекта.

---
+++ Collections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MostRepeatedWord
+{
+    public static void Main()
+    {
+        String line, word = "";
+        int count = 0, maxCount = 0;
+       // ArrayList words = new ArrayList();
+        var words = new List<string>();
+
+
+
+        //Opens file in read mode  
+        System.IO.StreamReader file = new System.IO.StreamReader(@"E:\TestFolder\Text11.txt");
+
+        //Reads each line  
+        while ((line = file.ReadLine()) != null)
+        {
+            String[] string1 = line.ToLower().Split(new Char[] { ',', '.', ' ', '-', }, StringSplitOptions.RemoveEmptyEntries);
+            //Adding all words generated in previous step into words  
+            foreach (String s in string1)
+            {
+                words.Add(s);
+            }
+        }
+
+        //Determine the most repeated word in a file 
+        for (int i = 0; i < words.Count; i++)
+        {
+            count = 1;
+            //Count each word in the file and store it in variable count  
+            for (int j = i + 1; j < words.Count; j++)
+            {
+                if (words[i].Equals(words[j]))
+                {
+                    count++;
+                }
+            }
+            //If maxCount is less than count then store value of count in maxCount   
+            //and corresponding word to variable word  
+            if (count > maxCount)
+            {
+                maxCount = count;
+                word = (string)words[i];
+            }
+        }
+
+        Console.WriteLine("Most repeated word: " + word);
+        file.Close();
+    }
+}
---
+++ Collections2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRepeatedWordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void FinDDuplicates(string filepath)
+        {
+            using (StreamReader reader = new StreamReader(@"E:\TestFolder\Text1.txt"))
+            {
+                int val = 0;
+                Dictionary<string, int> dWords = new Dictionary<string, int>();
+                string contents = reader.ReadToEnd();
+                string[] strArContents = contents.Split(new string[] { "", " ", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < strArContents.Length; i++)
+                {
+                    if (dWords.ContainsKey(strArContents[i]))
+                    {
+                        dWords.TryGetValue(strArContents[i], out val);
+                        dWords[strArContents[i]] = val + 1;
+
+                    }
+                    else
+                    {
+                        dWords.Add(strArContents[i], 1);
+                    }
+                }
+                var quer = from n in dWords where (n.Value > 0) select n;
+                foreach (var n in quer)
+                {
+                    Console.Write(n.Key + " : ");
+                    Console.WriteLine(n.Value);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
